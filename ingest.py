a2b8b3d0e927246365c60458d7dd673e6cabd015
Carson Mitteness <mitteness.c@northeastern.edu>
2025-03-17 23:38:07 -0400

    Fixed Small Error

--- ingest.py
+++ ingest.py
@@ -130,7 +130,7 @@ def main():
     clear_redis_store()
     create_hnsw_index()
 
-    process_pdfs("/Users/carson/Desktop/pratical_02/data")
+    process_pdfs(".../.data")
     print("\n---Done processing PDFs---\n")
     query_redis("What is the capital of France?")
 
